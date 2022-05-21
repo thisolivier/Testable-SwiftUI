956bc14cc2e4d22a1c17d65c2d9f24fe093a16d0
Olivier Butler <olivier@olivier.uk>
2022-05-21 14:19:06 +0100

    added some experimental code for handling overrding the environment

--- BitCoinz/BitCoinz/Environment.swift
+++ BitCoinz/BitCoinz/Environment.swift
@@ -8,8 +8,25 @@
 import Foundation
 
 struct Environment {
+    // If you wanted to pass in launch arguments via the ProcessInfo.processInfo.environment dictionary, look them up here
     static var shared: Environment = Environment()
+
+//    static func overrideEnvironment(with overrides: EnvironmentOverrides) {
+//        shared = Environment(
+//            coinsApiHost: overrides.coinsApiHost ?? shared.coinsApiHost,
+//            coinsApiSchema: overrides.coinsApiSchema ?? shared.coinsApiSchema
+//        )
+//    }
+//
+//    static func resetEnvironment() {
+//        shared = Environment()
+//    }
 
     var coinsApiHost: String = "psp-merchantpanel-service-sandbox.ozanodeme.com.tr"
     var coinsApiSchema: String = "https"
 }
+
+//struct EnvironmentOverrides {
+//    var coinsApiHost: String?
+//    var coinsApiSchema: String?
+//}
