8b257038c64f230891138a7e4fc5b4a0c2c9360d
Olivier Butler <olivier@olivier.uk>
2022-05-28 20:06:49 +0100

    Better store

--- BitCoinz/BitCoinz/Database/CoinPriceStore.swift
+++ BitCoinz/BitCoinz/Database/CoinPriceStore.swift
@@ -7,30 +7,46 @@
 
 import Foundation
 
+struct PersistentCoinData: Codable {
+    let date: Date
+    let coin: Coin
+}
+
 protocol CoinPriceStorable {
     /** Will store to disk the date queried, and coin price when queried, for all coins passed in. Appends to existing data. **/
     func save(data: [Coin])
     /** Retreives from disk an array of all previously saved date & price info for a given coin id  **/
-    func retrieve(for uuid: String, completion: @escaping ([String]) -> Void)
+    func retrieve(for uuid: String, completion: @escaping ([PersistentCoinData]) -> Void)
 }
 
 // Could be made generic: Any object with a UUID (identifiable) and a .description/string value could be stored & retreived.
 class CoinPriceStore: CoinPriceStorable {
+
+    private let defaults = UserDefaults.standard
+
+    private func persistentData(for coinId: String) -> [PersistentCoinData] {
+        guard
+            let savedData = defaults.object(forKey: coinId) as? Data,
+            let savedArray = try? PropertyListDecoder().decode(
+                [PersistentCoinData].self,
+                from: savedData
+            )
+        else { return [] }
+        return savedArray
+    }
+
     func save(data: [Coin]) {
         DispatchQueue.main.async {
-            let defaults = UserDefaults.standard
-            let date = Date()
             for coin in data {
-                var savedArray = defaults.object(forKey: coin.id) as? [String] ?? [String]()
-                savedArray.append(("\(date) - \( coin.price)"))
-                defaults.set(savedArray, forKey: coin.id)
+                var savedArray = self.persistentData(for: coin.id)
+                savedArray.append(PersistentCoinData(date: Date(), coin: coin))
+                let encoded = try! PropertyListEncoder().encode(savedArray)
+                self.defaults.set(encoded, forKey: coin.id)
             }
         }
     }
     
-    func retrieve(for id: String, completion: @escaping ([String]) -> Void) {
-        let defaults = UserDefaults.standard
-        let savedArray = defaults.object(forKey: id) as? [String] ?? [String]()
-        completion(savedArray)
+    func retrieve(for id: String, completion: @escaping ([PersistentCoinData]) -> Void) {
+        completion(persistentData(for: id))
     }
 }
